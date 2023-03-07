c225fdb805e7c1de0cc3c9e62ea4e4760a42ccbc
Gadielc15c <gadielcascante152001@gmail.com>
2023-03-06 20:33:40 -0400

    Changed LBL PRINCIPAL NAME

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('mamaguevo'),
         ),
         body:  Center(
           //
